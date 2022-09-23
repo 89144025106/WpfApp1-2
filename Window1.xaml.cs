a961780f57bb68029dda2eb57f5bf76ded42c8bf
Кристина Гуляева <kristinagul.24@gmail.com>
2022-09-23 15:36:26 +1000

    переход на новое окно

--- Window1.xaml.cs
+++ Window1.xaml.cs
@@ -10,28 +10,18 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace WpfApp1_Гуляева_Кристина_2
 {
     /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
+    /// Логика взаимодействия для Window1.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class Window1 : Window
     {
-        public MainWindow()
+        public Window1()
         {
             InitializeComponent();
         }
-
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            MessageBox.Show("Привет группа ИСП"); 
-        
-
-        
-        }
     }
 }
- 
